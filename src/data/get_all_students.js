424e25e8884288ed9200dbedc8008bbcbf73e785
christophersulayao <christoper.sulayao@gmail.com>
2019-02-08 15:44:50 -0800

    Completed displaying student data from dummy data

---
+++ src/data/get_all_students.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id:'001',
+        name: 'Donald Trump',
+        course: 'Face Painting 101',
+        grade: 10
+    },
+    {
+        id:'002',
+        name: 'Example Name',
+        course: 'Sample Course 51',
+        grade: 40
+    },
+    {
+        id:'003',
+        name: 'Chris Sulayao',
+        course: 'Suck at coding 33',
+        grade: 80
+    },
+    {
+        id:'004',
+        name: 'Kat Williams.',
+        course: 'Pimpin 303',
+        grade: 100
+    }
+];
